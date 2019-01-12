20a008b0867c1272112763dd0dae0f007acbfbc5
bulletsman04 <bulletsman04@gmail.com>
2019-01-12 23:33:38 +0100

    Partly changed simulation icons

--- VRP/VRP/wwwroot/simulator.js
+++ VRP/VRP/wwwroot/simulator.js
@@ -20,7 +20,6 @@
 
     Run() {
         this.Couriers.forEach(courier => courier.BindMarker());
-        this.AssignActionsToButtons();
         this.EnableButtons();
         this.Play();
         $("#startSimulation").attr("disabled", "disabled");
@@ -29,14 +28,14 @@
     Play() {
         this.Loop = setInterval(() => this.UpdateFrame(), 1000 / this.FramesPerSecond);
 
-        $("#pauseResumeButton").html('Pause');
+        $("#pauseResumeButton").attr('src', "simulation-icons/play_partly_active.svg");
         $("#pauseResumeButton").off('click');
         $("#pauseResumeButton").on('click', this.Pause.bind(this));
     }
 
     Pause() {
         clearInterval(this.Loop);
-        $("#pauseResumeButton").html('Resume');
+        $("#pauseResumeButton").attr('src', "simulation-icons/pause_partly_active.svg");
         $("#pauseResumeButton").off('click');
         $("#pauseResumeButton").on('click', this.Play.bind(this));
     }
@@ -65,15 +64,33 @@
 
     }
 
+    RemoveActionsFromButtons() {
+        $("#pauseResumeButton").off('click');
+        $("#stopButton").off('click');
+        $("#speedUpButton").off('click');
+        $("#slowDownButton").off('click');
+    }
+
     EnableButtons() {
-        $("#pauseResumeButton").attr('src', "simulation-icons/pause_partly_active.svg");
+        $("#pauseResumeButton").attr('src', "simulation-icons/pause_partly_active.svg")
+            .hover(this.SetImage.bind($("#pauseResumeButton"), "simulation-icons/pause_active.svg")
+                );
         $("#stopButton").attr('src', "simulation-icons/stop_partly_active.svg");
         $("#speedUpButton").attr('src', "simulation-icons/speedup_partly_active.svg");
         $("#slowDownButton").attr('src', "simulation-icons/speeddown_partly_active.svg");
+        this.AssignActionsToButtons();
     }
 
     DisableButtons() {
-        $(".simulation-panel").find(".btn").attr("disabled","disabled");
+        $("#pauseResumeButton").attr('src', "simulation-icons/pause_inactive.svg");
+        $("#stopButton").attr('src', "simulation-icons/stop_inactive.svg");
+        $("#speedUpButton").attr('src', "simulation-icons/speedup_inactive.svg");
+        $("#slowDownButton").attr('src', "simulation-icons/speeddown_inactive.svg");
+        this.RemoveActionsFromButtons();
+    }
+
+    SetImage(path) {
+        this.attr('src', path);
     }
 
 }
